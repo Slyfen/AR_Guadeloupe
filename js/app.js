4a2f66022b75686c2917eacd4a9e6f050fbea700
Slyfen <90706301+Slyfen@users.noreply.github.com>
2021-12-09 13:04:38 +0100

    Init_ceramique

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/Test_AR_Rotate.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/Test_ar_ceramique.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
